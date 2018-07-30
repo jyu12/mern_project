e470165b28cbbe162647d8f429f8c334a2b08c50
jason <xingy@pdx.edu>
2018-07-30 11:49:17 -0700

    Add profile model

---
+++ models/Profile.js
@@ -0,0 +1,121 @@
+const mongoose = require('mongoose');
+const Schema = Mongoose.Schema;
+
+const ProfileSchema = new Schema({
+    // some data are handled by Redux
+    user: {
+        // Sets the relationship to a user's id
+        type: Schema.Types.ObjectId,
+        // reference the users collection
+        ref: 'users'
+    },
+    // validation on the back-end also
+    handle: {
+        type: String,
+        required: true,
+        max: 40
+    },
+    company: {
+        type: String,
+    },
+    website: {
+        type: String,
+    },
+    location: {
+        type: String,
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    skills: {
+        type: [String],
+        required: true
+    },
+    bio: {
+        type: String,
+
+    },
+    githubUserName: {
+        type: String,
+    },
+    experience: [
+        {
+            title: { 
+                type: String,
+                required: true,
+            },
+            company: {
+                type: String,
+                required: true
+            },
+            location: {
+                type: String
+            },
+            from: {
+                type: Date,
+                required: true
+            },
+            to: {
+                type: Date
+            },
+            current: {
+                type: Boolean,
+                default: false
+            },
+            description: {
+                type: String
+            }
+        }
+    ],
+    education: [
+        {
+            school: { 
+                type: String,
+                required: true,
+            },
+            degree: {
+                type: String,
+                required: true
+            },
+            fieldofstudy: {
+                type: String,
+                required: true
+            },
+            from: {
+                type: Date,
+                required: true
+            },
+            to: {
+                type: Date
+            },
+            current: {
+                type: Boolean,
+                default: false
+            },
+            description: {
+                type: String
+            }
+        }
+    ],
+    social: {
+        youtube: {
+            type: String
+        },
+        twitter: {
+            type: String
+        },
+        linkedin: {
+            type: String
+        },
+        facebook: {
+            type: String
+        }
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema)
